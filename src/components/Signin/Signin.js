c6e8fbe6c5d82ee3492400858bc2604c95441d5d
Narayan soni <narayansoni854@gmail.com>
2022-04-24 22:26:47 +0530

    update frontend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://obscure-bastion-20348.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
