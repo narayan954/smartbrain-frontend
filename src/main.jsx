90d7716ce5d27aa41eee7ad645c19a615a67a292
Narayan soni <narayansoni3369@gmail.com>
2022-10-10 01:06:34 +0530

    migration to vite

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'tachyons';
+
+ReactDOM.render(<App />, document.getElementById('root'));
