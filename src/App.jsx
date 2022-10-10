00e2ca8f8d8c08206a6b77c6457aca10626eeaaa
billy le <lebilly87@gmail.com>
2022-10-09 21:00:39 -0700

    #10: updated logic to detect multiple faces in image and added error handlers for invalid urls

--- src/App.jsx
+++ src/App.jsx
@@ -84,8 +84,9 @@ const particlesOptions = {
 
 const initialState = {
   input: "",
+  error: "",
   imageUrl: "",
-  box: {},
+  boxes: [],
   route: "signin",
   isSignedIn: false,
   user: {
@@ -116,55 +117,95 @@ class App extends Component {
   };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
+
+    let boxes = [];
+    if (data?.outputs?.length) {
+      for (const output of data.outputs) {
+        for (const region of output.data.regions) {
+          const {
+            left_col: leftCol,
+            top_row: topRow,
+            right_col: rightCol,
+            bottom_row: bottomRow,
+          } = region.region_info.bounding_box;
+
+          boxes.push({
+            leftCol: leftCol * width,
+            topRow: topRow * height,
+            rightCol: width - rightCol * width,
+            bottomRow: height - bottomRow * height,
+          });
+        }
+      }
+    }
+
+    return boxes;
   };
 
-  displayFaceBox = (box) => {
-    this.setState({ box });
+  displayFaceBox = (boxes) => {
+    this.setState({ boxes });
   };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
 
+  onError = (err) => {
+    console.log(err);
+    this.setState({ error: err.message });
+  };
+
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    fetch("https://obscure-bastion-20348.herokuapp.com/imageurl", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        input: this.state.input,
-      }),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        if (response) {
-          fetch("https://obscure-bastion-20348.herokuapp.com/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
+    let url = "";
+    try {
+      url = new URL(this.state.input);
+
+      this.setState({ imageUrl: url.href, error: "" }, () => {
+        fetch("https://obscure-bastion-20348.herokuapp.com/imageurl", {
+          method: "post",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            input: this.state.imageUrl,
+          }),
+        })
+          .then(async (response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw Error(await response.json());
+            }
           })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            })
-            .catch(console.log);
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-      })
-      .catch((err) => console.log(err));
+          .then((response) => {
+            if (response) {
+              fetch("https://obscure-bastion-20348.herokuapp.com/image", {
+                method: "put",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                  id: this.state.user.id,
+                }),
+              })
+                .then((response) => {
+                  if (response.ok) {
+                    return response.json();
+                  } else {
+                    throw Error("There was an error");
+                  }
+                })
+                .then((count) => {
+                  this.setState(Object.assign(this.state.user, { entries: count }));
+                })
+                .catch(this.onError);
+            }
+            this.displayFaceBox(this.calculateFaceLocation(response));
+          })
+          .catch(this.onError);
+      });
+    } catch (err) {
+      this.setState({ error: "Invalid URL" });
+    }
   };
 
   onRouteChange = (route) => {
@@ -177,34 +218,26 @@ class App extends Component {
   };
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation
-          isSignedIn={isSignedIn}
-          onRouteChange={this.onRouteChange}
-        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
+              error={this.state.error}
             />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
+            <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
           </div>
         ) : route === "signin" ? (
           <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register
-            loadUser={this.loadUser}
-            onRouteChange={this.onRouteChange}
-          />
+          <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )}
       </div>
     );
